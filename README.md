42cff928c30f7b1e95e45506198cd9a52507a593
Elena Gorshenina <elenkagorshenina19.07.14@gmail.com>
2021-09-18 22:58:09 +0300

    Added .appveyor.yml, README.md and adited TestPostmanEcho

---
+++ README.md
@@ -0,0 +1,3 @@
+# AQA2.3 [![Build status](https://ci.appveyor.com/api/projects/status/5jmq3f9o21scminq?svg=true)](https://ci.appveyor.com/project/GorsheninaElena/aqa2-3)
+
+
--- src/test/java/ru/netology/TestPostmanEcho.java
+++ src/test/java/ru/netology/TestPostmanEcho.java
@@ -1,8 +1,14 @@
 package ru.netology;
 
+import io.restassured.http.ContentType;
+import io.restassured.specification.Argument;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 
 public class TestPostmanEcho {
 
@@ -19,7 +25,11 @@ public class TestPostmanEcho {
                 // Проверки
                 .then()
                 .statusCode(200)
-                //.body(/* --> ваша проверка здесь <-- */)
+                .body("data", equalTo("some data"))
+                .contentType(ContentType.JSON)
+                .headers("Content-Length", "424")
+                .headers("Connection", "keep-alive")
         ;
     }
+
 }
